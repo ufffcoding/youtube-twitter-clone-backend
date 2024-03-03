aadbae9bff752a43a3aef07964867c38639dd5a3
Deepanshuverma00 <deepanshusv3@gmail.com>
2024-03-03 16:13:17 +0530

    add user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema(
+  {
+    videoFile: {
+      type: String,
+    },
+    thumbnail: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    duration: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate());
+
+export const Video = mongoose.model("Video", videoSchema);
